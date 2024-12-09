2a37377af1d749360f4d98570e1aad954b7df1d2
Griotold <gotjd9773@naver.com>
2024-12-09 15:56:13 +0900

    User 엔티티 - 도메인 규칙을 엔티티에 구현하기
    1. 입력으로 "010-1234-5678" 와 같이 전화번호가 입력되지만, 엔티티 내에서 "01012345678" 로 변환해준다.

---
+++ src/main/java/com/example/ddd/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.example.ddd.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+//TODO: DDD를 사용할 경우 access 제한 설정을 권장드립니다.
+@Entity
+@Table(name = "users")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    // TODO: 입력으로 전화번호에 "-"가 들어오지만 공백으로 바꾼다는 도메인 규칙
+    private static final String PHONE_NUMBER_TRANSFER_TARGET = "-";
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    private Boolean isManufacture;
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public static User create(String name, String email, String phoneNumber) {
+        User user = User.builder()
+                .name(name)
+                .email(email)
+                .build();
+        //TODO: Static Method 로 구현한 규칙의 경우 builder 생성 이후 호출로 구현 가능합니다.
+        user.transferPhoneNumberFormat(phoneNumber);
+        return user;
+    }
+
+    private void transferPhoneNumberFormat(String phoneNumber) {
+        this.phoneNumber = phoneNumber.replace(PHONE_NUMBER_TRANSFER_TARGET, "");
+    }
+}
