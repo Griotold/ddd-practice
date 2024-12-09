a733c409f841e5fa5ec0d01c38747e724e09c02e
Griotold <gotjd9773@naver.com>
2024-12-09 19:32:51 +0900

    ProductImage

--- src/main/java/com/example/ddd/domain/product/ProductImage.java
+++ src/main/java/com/example/ddd/domain/product/ProductImage.java
@@ -13,4 +13,24 @@ public class ProductImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String url;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private ProductDetail productDetail;
+
+    public static ProductImage create(ProductDetail productDetail, String name, String url) {
+        return ProductImage.builder()
+                .productDetail(productDetail)
+                .name(name)
+                .url(url)
+                .build();
+    }
+
+    public void update(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 }
