ccba211fe263b1a8082f5eced61dbdb01b81f7c9
Griotold <gotjd9773@naver.com>
2024-12-09 16:19:37 +0900

    Product와 ProductDetail - 미완성

--- src/main/java/com/example/ddd/domain/product/Product.java
+++ src/main/java/com/example/ddd/domain/product/Product.java
@@ -1,4 +1,30 @@
 package com.example.ddd.domain.product;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private ProductDetail productDetail;
+
+    // TODO: Entity 에 Value Object 연결 예시
+    @Embedded
+    private AuditingDate dateInfo;
+
+    private String manufacturedBy;
 }
--- src/main/java/com/example/ddd/domain/product/ProductDetail.java
+++ src/main/java/com/example/ddd/domain/product/ProductDetail.java
@@ -1,4 +1,26 @@
 package com.example.ddd.domain.product;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
 public class ProductDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_detail_id")
+    private Long id;
+
+    @Column(name = "retail_price")
+    private Long retailPrice;
+
+    @Column(name = "supply_price")
+    private Long supplyPrice;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
